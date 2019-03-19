50630a54d83ffd45ed9db85f93b62db4ad6f2266
Rajaram683 <48674324+Rajaram683@users.noreply.github.com>
2019-03-19 11:08:35 +0530

    changed

--- SpringProject/src/spring/pull.java
+++ SpringProject/src/spring/pull.java
@@ -2,7 +2,8 @@ package spring;
 
 public class pull {
 	public static void main(String[] args) {
-System.out.println("hello world");		
+System.out.println("hello world");
+		System.out.println("hello world");
 	}
 
 }
